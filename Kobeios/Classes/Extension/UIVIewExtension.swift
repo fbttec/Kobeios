7e95bea73484f0c84dd7e2c28b4e9bdc6ed0d62f
Thomas Delgado <thomas.ddelgado@gmail.com>
2018-06-22 15:48:42 -0300

    fix bug keyboard extension

--- Kobeios/Classes/Extension/UIVIewExtension.swift
+++ Kobeios/Classes/Extension/UIVIewExtension.swift
@@ -10,15 +10,89 @@ import UIKit
 
 public extension UIView {
     
-    func roundedCornerColor(borderColor: UIColor = .clear, borderWidth: CGFloat = 0, radius: CGFloat) {
+    func roundCorners(_ corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: self.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        self.layer.mask = mask
+    }
+    
+    func roundCorners(corners:UIRectCorner, radius: CGFloat) {
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius)).cgPath
+        layer.mask = maskLayer
+    }
+    
+    func roundCorners(corners:UIRectCorner, radius: CGFloat, roundedRect: CGRect)
+    {
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = UIBezierPath(roundedRect: roundedRect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius)).cgPath
+        layer.mask = maskLayer
+    }
+    
+    func roundCornerColor(borderColor: UIColor = .clear, borderWidth: CGFloat = 0, radius: CGFloat) {
         layer.cornerRadius = radius
-        layer.borderColor = borderColor.cgColor
-        layer.borderWidth = borderWidth
+//        layer.borderColor = borderColor.cgColor
+//        layer.borderWidth = borderWidth
         clipsToBounds = true
     }
     
-    func roundedSideBorder() {
-        roundedCornerColor(radius: frame.size.height/2.0)
+    @IBInspectable var circularCorner: Bool {
+        get {
+            return self.circularCorner
+        }
+        set {
+            if newValue == true {
+                self.circularCorners()
+            } else {
+                layer.cornerRadius = 0
+            }
+        }
+    }
+    
+    @IBInspectable var roundedCorner: Bool {
+        get {
+            return self.roundedCorner
+        }
+        set {
+            if newValue == true {
+                self.roundCorners()
+            } else {
+                layer.cornerRadius = 0
+            }
+        }
+    }
+    
+    func addShadow(opacity: Float = 1, shadowRadius: CGFloat = 7, cornerRadius: CGFloat = 5, corners: CACornerMask = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]) {
+        backgroundColor = UIColor.white
+        layer.shadowOffset = CGSize.zero
+        layer.shadowColor = UIColor(red:0, green:0, blue:0, alpha:0.1).cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowRadius = shadowRadius
+        layer.cornerRadius = cornerRadius
+        if #available(iOS 11.0, *) {
+            layer.maskedCorners = corners
+        }
+    }
+    
+    func circularCorners() {
+        roundCornerColor(radius: frame.size.width/2.0)
+    }
+    
+    func roundCorners() {
+        roundCornerColor(radius: frame.size.height/2.0)
+    }
+    
+    func roundCorners(color: UIColor) {
+        roundCornerColor(borderColor: color, borderWidth: 1.0, radius: frame.size.height/2.0)
+    }
+    
+    func roundCorners(radius: CGFloat) {
+        roundCornerColor(borderColor: .clear, borderWidth: 1.0, radius: radius)
+    }
+    
+    func roundCornersLight() {
+        roundCornerColor(borderWidth: 1.0, radius: 6.0)
     }
     
 }

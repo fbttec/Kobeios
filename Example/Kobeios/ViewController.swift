3ed3a5274926329add591d4d71615917ae323913
Thomas Delgado <thomas.ddelgado@gmail.com>
2018-07-06 18:42:25 -0300

    add kobe component textfield labeled

--- Example/Kobeios/ViewController.swift
+++ Example/Kobeios/ViewController.swift
@@ -9,16 +9,24 @@
 import UIKit
 import Kobeios
 
-class ViewController: UIViewController, Stringable {
 
+class ViewController: UIViewController, Stringable {
+    
+    @IBOutlet weak var txtField: TextfieldLabeled!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
         
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func buttonTouch(_ sender: Any) {
+        txtField.showLabel( txtField.labelHidden)
+    }
+    
 }
---
+++ Kobeios/Classes/UIKit/TextfieldLabeled.swift
@@ -0,0 +1,102 @@
+//
+//  TextfieldLabeled.swift
+//  Kobeios
+//
+//  Created by Thomas Delgado on 05/07/18.
+//
+
+import UIKit
+
+@IBDesignable
+public class TextfieldLabeled: UITextField {
+    
+    @IBInspectable
+    public var underlineColor: UIColor = UIColor.darkGray {
+        didSet {
+            configureUnderline(frame: frame)
+        }
+    }
+    
+    var underline: UIView?
+    public var label: UILabel?
+    
+    @IBInspectable
+    public var labelHidden: Bool = true {
+        didSet {
+            configureLabel()
+            configureUnderline(frame: frame)
+        }
+    }
+    
+    @IBInspectable
+    public var labelColor: UIColor = UIColor.clear {
+        didSet {
+            label?.textColor = labelColor
+        }
+    }
+    
+    @IBInspectable
+    public var labelText: String? {
+        didSet {
+            label?.text = labelText
+        }
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLabel()
+        configureUnderline(frame: frame)
+        self.borderStyle = .none
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureLabel()
+        configureUnderline(frame: frame)
+        self.borderStyle = .none
+    }
+    
+    func configureUnderline(frame: CGRect) {
+        if underline == nil {
+            underline = UIView()
+        }
+        underline?.backgroundColor = labelHidden ? underlineColor : labelColor
+        underline?.frame = CGRect(x: 0,
+                                 y: frame.size.height - 1,
+                                 width: frame.size.width, height: 1)
+        self.addSubview(underline!)
+    }
+    
+    func configureLabel() {
+        if label == nil {
+            label = UILabel()
+        }
+        label?.frame = CGRect.init(origin: CGPoint(x: frame.origin.x,
+                                                   y: frame.origin.y + frame.size.height),
+                                   size: CGSize(width: frame.size.width, height: frame.size.height))
+        label?.textAlignment = .right
+        label?.minimumScaleFactor = 0.5
+        label?.lineBreakMode = .byTruncatingHead
+        label?.text = labelText
+        label?.isHidden = false
+        label?.alpha = labelHidden ? 0 : 1
+        self.superview?.addSubview(label!)
+    }
+    
+    public func showLabel(_ show: Bool) {
+        if show {
+            label?.alpha = 0
+            UIView.animate(withDuration: 0.5) {
+                self.label?.alpha = 1
+                self.underline?.backgroundColor = self.labelColor
+            }
+        } else {
+            label?.alpha = 1
+            UIView.animate(withDuration: 0.5) {
+                self.label!.alpha = 0
+                self.underline!.backgroundColor = self.underlineColor
+            }
+        }
+        self.labelHidden = !show
+    }
+}

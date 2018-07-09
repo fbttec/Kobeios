39aead663a60ab7d58777bc62f7df3036804f534
Thomas Delgado <thomas.ddelgado@gmail.com>
2018-07-09 16:58:34 -0300

    add alertExtension with better names

---
+++ Kobeios/Classes/Common/StringMessages.swift
@@ -0,0 +1,13 @@
+//
+//  StringMessages.swift
+//  Kobeios
+//
+//  Created by Thomas Delgado on 09/07/18.
+//
+
+import Foundation
+
+public struct StringMessages {
+    static var okButton = "Ok"
+    static var cancelButton = "Cancel"
+}

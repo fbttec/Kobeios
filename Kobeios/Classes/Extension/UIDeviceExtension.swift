87b14fcaf67929e6b82c6244d13793c8c5d0cc9e
Thomas Delgado <thomas.ddelgado@gmail.com>
2018-08-29 18:27:03 -0300

    add device utils

---
+++ Kobeios/Classes/Extension/UIDeviceExtension.swift
@@ -0,0 +1,29 @@
+//
+//  UIDeviceExtension.swift
+//  Kobeios
+//
+//  Created by Thomas Delgado on 28/08/18.
+//
+
+import Foundation
+
+public extension UIDevice {
+    static var isIphoneX: Bool {
+        var modelIdentifier = ""
+        if isSimulator {
+            modelIdentifier = ProcessInfo.processInfo.environment["SIMULATOR_MODEL_IDENTIFIER"] ?? ""
+        } else {
+            var size = 0
+            sysctlbyname("hw.machine", nil, &size, nil, 0)
+            var machine = [CChar](repeating: 0, count: size)
+            sysctlbyname("hw.machine", &machine, &size, nil, 0)
+            modelIdentifier = String(cString: machine)
+        }
+        
+        return modelIdentifier == "iPhone10,3" || modelIdentifier == "iPhone10,6"
+    }
+    
+    static var isSimulator: Bool {
+        return TARGET_OS_SIMULATOR != 0
+    }
+}

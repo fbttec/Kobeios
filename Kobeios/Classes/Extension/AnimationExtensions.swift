a307f38a9c051c5c544b3add70cbe05ff7740287
Thomas Delgado <thomas.ddelgado@gmail.com>
2018-09-06 15:14:56 -0300

    add transition text

---
+++ Kobeios/Classes/Extension/AnimationExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  AnimationExtensions.swift
+//  Kobeios
+//
+//  Created by Thomas Delgado on 06/09/18.
+//
+
+import Foundation
+
+
+extension UIView {
+// Usage: insert view.fadeTransition right before changing content
+    func fadeTransition(_ duration:CFTimeInterval) {
+        let animation = CATransition()
+        animation.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseInEaseOut)
+        animation.type = kCATransitionFade
+        animation.duration = duration
+        layer.add(animation, forKey: kCATransitionFade)
+    }
+    
+}
